34d3c5245f4766381cf87e144bb235c7cbfabe2d
Lyutik <lyutikov.viktor.96@mail.ru>
2022-07-28 23:59:16 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alien Invasion."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана
+        self.screen_width = 1600
+        self.screen_heigh = 900
+        self.bg_color = (51, 140, 229)
+
+        # Настройки корабля
+        self.ship_speed = 1.5
+        self.ship_limit = 1
+
+        # Параметры снаряда
+        self.bullet_speed = 1.5
+        self.bullet_width = 2.8
+        self.bullet_height = 25
+        self.bullet_color = (211,211,211)
+        self.bullets_allowed = 3
+
+        # Настройки пришельцев
+        self.alien_speed = 0.6
+        self.fleet_drop_speed = 50
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 0.6
---
+++ ship.py
@@ -0,0 +1,44 @@
+import pygame
+
+
+class Ship():
+    """Класс для управления кораблем."""
+
+    def __init__(self, ai_game):
+        """Инициализирует корабль и задает его начальную позицию."""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Загружает изображение корабля и получает прямоугольник.
+        self.image = pygame.image.load('images/ship/mig-29_strij.bmp')
+        self.rect = self.image.get_rect()
+        # Каждый новый корабль появляется у нижнего края экрана.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флагов."""
+        # Обновляется атрибут x, не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Рисует корабль в текущей позиции."""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Размещает корабль в центре нижней стороны."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
